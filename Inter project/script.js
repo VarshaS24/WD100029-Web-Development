8121d6cb9bc0bfb008cb0d8ed924cf507f7f2c14
Varsha S <96822806+VarshaS24@users.noreply.github.com>
2025-01-11 20:07:55 +0530

    Delete Inter project/script.js

--- Inter project/script.js
+++
@@ -1,8 +0,0 @@
-
-
-var typed=new Typed(".typing",{
-    strings:["Web Developer","Front-end Developer","Back-end Developer","Video Editor"],
-    typeSpeed:100,
-    Backspeed:60,
-    loop:true
-})
